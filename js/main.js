677b0ceb33363a1212d815a8be25124a50ea8a32
Alexander Bogish <ego.cogitare@gmail.com>
2017-09-20 08:59:16 -0700

    ...

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-// var slider = new Swiper('.swiper-container', {
-//   direction: 'horizontal',
-//   slidesPerView: 1,
-//   pagination: '.swiper-pagination',
-//   paginationClickable: true,
-//   autoplay: 4000,
-//   loop: true,
-//   spaceBetween: 0,
-//   mousewheelControl: false,
-//   speed: 1000
-// });
 
 $(document).ready(function() {
-
+  if ($('.swiper-wrapper .swiper-slide').length > 1) {
+    var slider = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      slidesPerView: 1,
+      pagination: '.swiper-pagination',
+      paginationClickable: true,
+      autoplay: 4000,
+      loop: true,
+      spaceBetween: 0,
+      mousewheelControl: false,
+      speed: 1000
+    });
+  }
 });
