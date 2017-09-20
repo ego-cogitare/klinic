c53ea0f355b3c80c59c195a846977c994016260e
Alexander Bogish <ego.cogitare@gmail.com>
2017-09-20 07:26:08 -0700

    ...

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// var slider = new Swiper('.swiper-container', {
+//   direction: 'horizontal',
+//   slidesPerView: 1,
+//   pagination: '.swiper-pagination',
+//   paginationClickable: true,
+//   autoplay: 4000,
+//   loop: true,
+//   spaceBetween: 0,
+//   mousewheelControl: false,
+//   speed: 1000
+// });
+
+$(document).ready(function() {
+
+});
